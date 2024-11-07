712ea92bb8521f137d47d46a0acaf371a0558ce1
aafxr <alexandral.fed23@gmail.com>
2024-11-07 16:01:01 +0700

    custom user type

--- local/media/js/CUserTypeColor.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#colorpickerHolder').ColorPicker({
-        flat: true,
-        color: $('#colorpickerHolderInput').val(),
-        onChange: function (hsb, hex, rgb) {
-            $('#colorpickerHolderInput').val(hex);
-        }
-    });
-});
