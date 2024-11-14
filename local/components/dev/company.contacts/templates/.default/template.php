5a990d512f6d2afe30a788bbd3ca13c67142ada6
aafxr <alexandral.fed23@gmail.com>
2024-11-14 08:47:03 +0700

    company.contacts template change

--- local/components/dev/company.contacts/templates/.default/template.php
+++ local/components/dev/company.contacts/templates/.default/template.php
@@ -76,8 +76,6 @@ $editeMode = boolval($arResult['EDITE_MODE'])
         <?php endforeach; ?>
     </div>
 </div>
-<<<<<<< HEAD
-
     <script>
         BX.ready(() => {
             let node = document.querySelector('[data-cid="<?=$arResult['USER_FIELD_NAME'];?>"]')
@@ -102,25 +100,4 @@ $editeMode = boolval($arResult['EDITE_MODE'])
             }
         })
     </script>
-=======
-<script>
-    BX.ready(() => {
-        let node = document.querySelector('[data-cid="<?=$arResult['USER_FIELD_NAME'];?>"]')
-        if (node) {
-            let titleNode = node.querySelector(".ui-entity-editor-block-title")
-            titleNode.style.display = "none"
-            node.addEventListener('click', (e) => {
-                const el = e.target.closest('.contact-block')
-                if(el && el.hasAttribute('data-contact-id')){
-                    const contact_id = el.getAttribute('data-contact-id')
-                    const title = "Контакт " + el.getAttribute('data-contact-name')
-                    const contact_url = window.location.origin + '/local/contact/contact_edite.php?contact_id=' + contact_id + '&IFRAME=Y'
-                    const dialog = new BX.CDialog({title, contact_url});
-                    dialog.Show()
-                }
-                return false
-            })
-        }
-    })
-</script>
->>>>>>> c2d379ccfaf3062d2fdcc9a8f7f1ff8ee75ba8ef
+
