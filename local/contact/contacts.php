ad70cec282c180aca067d2a24129438579a9ca45
aafxr <alexandral.fed23@gmail.com>
2024-12-04 13:30:27 +0700

    contact/contacts.php sync

--- local/contact/contacts.php
+++ local/contact/contacts.php
@@ -97,7 +97,7 @@ function getPrefferences($contact)
 ?>
 
 
-<div class="refloor-contacts" data-id="<?= $_GET['company_id'] ?>">
+<div class="refloor-contacts">
     <div class="crm-entity-widget-content-block-inner crm-entity-widget-inner">
         <div class="crm-entity-widget-content-block-inner-container">
             <div class="crm-entity-widget-content-block-title">
@@ -116,10 +116,9 @@ function getPrefferences($contact)
                     <div class="crm-entity-widget-client-box crm-entity-widget-participants-block">
                         <div class="crm-entity-widget-client-box-name-container">
                             <div class="crm-entity-widget-client-box-name-row">
-                                <a class="crm-entity-widget-client-box-name edit-contact"><?= $contact['NAME'] . ' ' . $contact['LAST_NAME']; ?></a>
+                                <a class="crm-entity-widget-client-box-name edit-contact" ><?= $contact['NAME'] . ' ' . $contact['LAST_NAME']; ?></a>
                                 <a href="/crm/contact/details/<?= $contact["ID"]; ?>/" class="form-client-card-link">
-                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16"
-                                         viewBox="0 0 24 24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 24 24">
                                         <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"></path>
                                     </svg>
                                 </a>
@@ -127,25 +126,24 @@ function getPrefferences($contact)
                         </div>
 
                         <div class="crm-entity-widget-client-box-position">
-                            <? if ($contact['POST'] != ""): ?>
+                            <? if($contact['POST'] != ""): ?>
                                 <?= $contact['POST']; ?>&nbsp;
                             <? endif ?>
-                            <? if ($contact['BIRTHDATE'] != ""): ?>
-                                <? $birthday = explode(" ", $contact['BIRTHDATE']); ?>
+                            <? if($contact['BIRTHDATE'] != ""): ?>
+                                <? $birthday = explode(" ",$contact['BIRTHDATE']); ?>
 
-                                день рождения: <?= $birthday[0] ?>
+                                день рождения: <?=$birthday[0]?>
                             <? endif ?>
                         </div>
                         <div class="crm-entity-widget-client-box-preferences refloor-contact-properties">
                             <?php
                             $preferences = getPrefferences($contact);
-                            foreach ($preferences as $k => $p) {
+                            foreach ($preferences as $k => $p){
                                 $name = $k;
                                 $ufFieldName = $p['NAME'];
                                 $checked = boolval($p['VALUE']);
                                 ?>
-                                <span class="refloor-contact-property"><?= $name; ?><span
-                                            class="<?= $checked ? 'yes' : 'no'; ?>"><?= $checked ? 'да' : 'нет'; ?></span></span>
+                                <span class="refloor-contact-property"><?= $name; ?><span class="<?= $checked ? 'yes' : 'no'; ?>"><?= $checked ? 'да' : 'нет'; ?></span></span>
                             <?php } ?>
                         </div>
                         <div class="crm-entity-widget-client-contact">
@@ -153,7 +151,8 @@ function getPrefferences($contact)
                                 <?php foreach ($contact['PHONE'] as $k => $phone): ?>
                                     <a
                                             class="crm-entity-widget-client-contact-item crm-entity-widget-client-contact-phone"
-                                            href="callto://<?= $phone['VALUE']; ?>"
+                                            href="tel://<?=preg_replace('/[^+\d]/', '', $phone['VALUE']);?>"
+                                            onclick="event.stopPropagation()"
                                     ><?= $phone['VALUE'] ?></a>
                                 <?php endforeach; ?>
                             <?php endif; ?>
@@ -161,7 +160,8 @@ function getPrefferences($contact)
                                 <?php foreach ($contact['EMAIL'] as $k => $email): ?>
                                     <a
                                             class="crm-entity-widget-client-contact-item crm-entity-widget-client-contact-phone"
-                                            href="mailto:<?= $email['VALUE']; ?>"
+                                            href="mailto:<?=$email['VALUE'];?>"
+                                            onclick="event.stopPropagation()"
                                     ><?= $email['VALUE'] ?></a>
                                 <?php endforeach; ?>
                             <?php endif; ?>
@@ -178,7 +178,7 @@ function getPrefferences($contact)
                             ?>
                         </div> */ ?>
                         <div class="refloor-comments">
-                            <?= $contact['COMMENTS']; ?>
+                            <?=$contact['COMMENTS'];?>
                         </div>
                     </div>
                 </div>
@@ -186,7 +186,11 @@ function getPrefferences($contact)
             <?php /* if($editeMode): ?>
                 <button class="ui-btn ui-btn-xs ui-btn-success add-contact-button">Добавить контакт</button>
             <?php endif; */ ?>
-            <a class="add-contact-button">Добавить контакт</a>
+            <?php if($arResult['COMPANY_ID'] == 0): ?>
+                <p>Для добавления контактов сохраните карточку компании</p>
+            <?php else: ?>
+                <a class="add-contact-button">Добавить контакт</a>
+            <?php endif ?>
         </div>
     </div>
     <script>
@@ -201,10 +205,11 @@ function getPrefferences($contact)
 
 
                 node.addEventListener('click', (e) => {
+                    e.preventDefault()
                     e.stopImmediatePropagation()
-                    if (e.target.closest('.form-client-card-link')) return
+                    if(e.target.closest('.form-client-card-link')) return
                     const el = e.target.closest('.contact-block')
-                    if (el && el.hasAttribute('data-contact-id')) {
+                    if(el && el.hasAttribute('data-contact-id')){
                         const contact_id = el.getAttribute('data-contact-id')
                         const title = "Контакт " + el.getAttribute('data-contact-name')
                         const content_url = '/local/contact/contact_edit.php?IFRAME=Y'
@@ -217,7 +222,7 @@ function getPrefferences($contact)
 
 
                 const newContactButton = node.querySelector('.add-contact-button')
-                if (newContactButton) {
+                if(newContactButton){
                     newContactButton.addEventListener('click', (e) => {
                         e.preventDefault()
                         const title = "Добавить контакт"
