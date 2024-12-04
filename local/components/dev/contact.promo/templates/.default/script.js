812e2f44e1fbd0f28891180fed4b28ca084dca37
aafxr <alexandral.fed23@gmail.com>
2024-12-04 12:32:55 +0700

    promo popup

--- local/components/dev/contact.promo/templates/.default/script.js
+++ local/components/dev/contact.promo/templates/.default/script.js
@@ -1,101 +1,98 @@
-BX.ready(() => {
-    const promoContainer = document.getElementById('contact-promo')
-    const dropDown = promoContainer.querySelector('.promo-dropdown')
-    const selectedPromo = {}
-
-    promoContainer.querySelectorAll('.dropdown-options-item.checked')
-        .forEach(e =>
-            selectedPromo[e.dataset.itemId] = {
-                UF_PROMO_ID: e.dataset.itemId,
-                UF_PROMO_VALUE: e.dataset.itemValue
-            }
-        )
-
-    let onClickOutside = false
-
-
-
-
-    const handleClickOutside = (e) => {
-        if(!e.target.closest('.promo-dropdown')){
-            document.removeEventListener('click', handleClickOutside)
-        }
-    }
-
-
-
-    dropDown.addEventListener('click', e => {
-        const nodeItem = e.target.closest('.dropdown-options-item')
-        if(nodeItem){
-            if(nodeItem.classList.contains('.checked')){
-                delete selectedPromo[nodeItem.dataset.itemId]
-            } else{
-                selectedPromo[nodeItem.dataset.itemId] = {
-                    UF_PROMO_ID: nodeItem.dataset.itemId,
-                    UF_PROMO_VALUE: nodeItem.dataset.itemValue
-                }
-            }
-            nodeItem.classList.toggle('checked ui-ctl-icon-clear')
-        } else {
-            dropDown.classList.toggle('open')
-        }
-
-
-        if(!onClickOutside) {
-            document.addEventListener("click", handleClickOutside)
-            onClickOutside = true
-        }
-    })
-
-
-
-
-
-
-    const promoList = promoContainer.querySelector('.promo-list')
-    const selectedPromoContainer = promoContainer.querySelector('.contact-promo-list')
-
-
-    promoContainer.querySelectorAll('.promo-list-item')
-        .forEach(e => {
-            if (selectedPromo[e.dataset.itemId]) e.classList.add('selected')
-        })
-
-
-    function getSelectedPromoTemplate(promo){
-        return `
-        <li
-                    class="ui-btn ui-btn-link contact-promo-list-item"
-                    data-item-id="${promo['UF_PROMO_ID']}"
-                    data-item-value="${promo['UF_PROMO_VALUE']}"
-            >
-                ${promo['UF_PROMO_VALUE']}
-            </li>
-        `
-    }
-
-
-
-    function updateSelectedList(){
-        selectedPromoContainer.innerHTML = Object.values(selectedPromo)
-            .map(e => getSelectedPromoTemplate(e))
-            .join('')
-    }
-
-
-
-
-    promoList.addEventListener('click', e => {
-        const promoItem = e.target.closest('.promo-list-item')
-        if (promoItem) {
-            const id = promoItem.dataset.itemId
-            const value = promoItem.dataset.itemValue
-            if (selectedPromo[id]) {
-                delete selectedPromo[id]
-            } else {
-                selectedPromo[id] = {UF_PROMO_ID: id, UF_PROMO_VALUE: value}
-            }
-            updateSelectedList()
-        }
-    })
-})
+// BX.ready(() => {
+//     const promoContainer = document.getElementById('contact-promo')
+//     const dropDown = promoContainer.querySelector('.promo-dropdown')
+//     const selectedPromo = {}
+//
+//     promoContainer.querySelectorAll('.dropdown-options-item.checked')
+//         .forEach(e =>
+//             selectedPromo[e.dataset.itemId] = {
+//                 UF_PROMO_ID: e.dataset.itemId,
+//                 UF_PROMO_VALUE: e.dataset.itemValue
+//             }
+//         )
+//
+//     let onClickOutside = false
+//
+//
+//     const handleClickOutside = (e) => {
+//         if (!e.target.closest('.promo-dropdown')) {
+//             document.removeEventListener('click', handleClickOutside)
+//         }
+//     }
+//
+//
+//     dropDown.addEventListener('click', e => {
+//         const nodeItem = e.target.closest('.dropdown-options-item')
+//         if (nodeItem) {
+//             if (nodeItem.classList.contains('.checked')) {
+//                 delete selectedPromo[nodeItem.dataset.itemId]
+//             } else {
+//                 selectedPromo[nodeItem.dataset.itemId] = {
+//                     UF_PROMO_ID: nodeItem.dataset.itemId,
+//                     UF_PROMO_VALUE: nodeItem.dataset.itemValue
+//                 }
+//             }
+//             nodeItem.classList.toggle('checked')
+//         } else {
+//             dropDown.classList.toggle('open')
+//         }
+//
+//
+//         if (!onClickOutside) {
+//             document.addEventListener("click", handleClickOutside)
+//             onClickOutside = true
+//         }
+//     })
+//
+//
+//
+//
+//
+// //
+// //     const promoList = promoContainer.querySelector('.promo-list')
+// //     const selectedPromoContainer = promoContainer.querySelector('.contact-promo-list')
+// //
+// //
+// //     promoContainer.querySelectorAll('.promo-list-item')
+// //         .forEach(e => {
+// //             if (selectedPromo[e.dataset.itemId]) e.classList.add('selected')
+// //         })
+// //
+// //
+// //     function getSelectedPromoTemplate(promo){
+// //         return `
+// //         <li
+// //                     class="ui-btn ui-btn-link contact-promo-list-item"
+// //                     data-item-id="${promo['UF_PROMO_ID']}"
+// //                     data-item-value="${promo['UF_PROMO_VALUE']}"
+// //             >
+// //                 ${promo['UF_PROMO_VALUE']}
+// //             </li>
+// //         `
+// //     }
+// //
+// //
+// //
+// //     function updateSelectedList(){
+// //         selectedPromoContainer.innerHTML = Object.values(selectedPromo)
+// //             .map(e => getSelectedPromoTemplate(e))
+// //             .join('')
+// //     }
+// //
+// //
+// //
+// //
+// //     promoList.addEventListener('click', e => {
+// //         const promoItem = e.target.closest('.promo-list-item')
+// //         if (promoItem) {
+// //             const id = promoItem.dataset.itemId
+// //             const value = promoItem.dataset.itemValue
+// //             if (selectedPromo[id]) {
+// //                 delete selectedPromo[id]
+// //             } else {
+// //                 selectedPromo[id] = {UF_PROMO_ID: id, UF_PROMO_VALUE: value}
+// //             }
+// //             updateSelectedList()
+// //         }
+// //     })
+// })
--- local/components/dev/contact.promo/templates/.default/template.php
+++ local/components/dev/contact.promo/templates/.default/template.php
@@ -1,40 +1,124 @@
 <?php
 
-\Bitrix\Main\UI\Extension::load("ui.forms");
-\Bitrix\Main\UI\Extension::load("ui.alerts");
 \Bitrix\Main\UI\Extension::load("ui.buttons");
-\Bitrix\Main\UI\Extension::load("ui.select");
 
 $promoList = $arResult['PROMO'] ?? [];
 $itemsList = [];
 
-foreach ($arResult['ITEMS'] as $item){
-    $itemsList[$item['UF_PROMO_ID']] = $item;
+foreach ($promoList as $k => $promo){
+    $res = CFile::GetPath(intval($promo['UF_PROMO_PHOTO'])) ?? '';
+    $promoList[$k]['UF_PROMO_PHOTO'] = $res;
 }
 
-$itemsList[1] = [];
-$itemsList[4] = [];
-
+foreach ($arResult['ITEMS'] as $item) {
+    $itemsList[$item['UF_PROMO_ID']] = $item;
+}
 ?>
 
     <div id="contact-promo" class="contact-promo">
-        <div class="ui-ctl ui-ctl-after-icon ui-ctl-dropdown promo-dropdown">
-            <div class="ui-ctl-after ui-ctl-icon-angle"></div>
-            <div class="ui-ctl-element"> Выбранная опция </div>
-            <div class="dropdown-options">
-                <? foreach ($promoList as $promo){
-                    $checked = boolval( $itemsList[$promo['UF_PROMO_ID']] );
-                ?>
-                    <div
-                        class="ui-ctl-element ui-ctl-after-icon dropdown-options-item <?= $checked ? 'ui-ctl-icon-clear checked' : ''; ?>"
-                        title="<?= $promo['UF_PROMO_VALUE']; ?>"
-                        data-item-id="<?= $promo['UF_PROMO_ID']?>"
-                        data-item-value="<?= $promo['UF_PROMO_VALUE']?>"
-                    > <?= $promo['UF_PROMO_VALUE']?> </div>
-                <? } ?>
+        <button class="ui-btn ui-btn-light-border promo-dropdown">Выбрать promo</button>
+    </div>
+    <script>
+        BX.ready(() => {
+            const promoContainer = document.getElementById('contact-promo')
+            const dropDown = promoContainer.querySelector('.promo-dropdown')
+            const promoList = <?= json_encode($promoList, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) ?>;
+            const selectedPromo = <?= json_encode($itemsList, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) ?>;
 
-            </div>
-        </div>
+            console.log(promoList)
+            console.log(selectedPromo)
 
-</div>
 
+            function getPopupContent() {
+                const listNode = document.createElement('div')
+                listNode.classList.add('promo-list')
+
+                for (const p of promoList){
+                    const itemNode = document.createElement('div')
+                    itemNode.classList.add('promo-list-item')
+                    if(selectedPromo[p.UF_PROMO_ID]) {
+                        itemNode.classList.add('promo-list-item--selected')
+                    }
+                    itemNode.innerHTML = `
+                        <div class="promo-list-item-photo">
+                            <img src="${p.UF_PROMO_PHOTO}" alt="${p.UF_PROMO_VALUE}" />
+                        </div>
+                        <div class="promo-list-item-name">${p.UF_PROMO_VALUE}</div>
+                    `
+
+                    itemNode.onclick = function(e){
+                        if(this.classList.contains('promo-list-item--selected')){
+                            delete selectedPromo[p.UF_PROMO_ID]
+                            this.classList.remove('promo-list-item--selected')
+                        } else{
+                            selectedPromo[p.UF_PROMO_ID] = {UF_PROMO_ID: p.UF_PROMO_ID}
+                            this.classList.add('promo-list-item--selected')
+                        }
+                    }
+
+                    listNode.appendChild(itemNode)
+                }
+                return listNode
+            }
+
+
+            function handleSavePromo(){
+                console.log(selectedPromo)
+            }
+
+
+            const promoPopup = new BX.PopupWindow(
+                "promo-popup-window",
+                dropDown,
+                {
+                    content: getPopupContent(),
+                    closeByEsc : true,
+                    autoHide : true,
+                    zIndex: 0,
+                    offsetLeft: 0,
+                    offsetTop: 0,
+                    draggable: {restrict: false},
+                    width: 800,
+                    buttons: [
+                        new BX.PopupWindowButton({
+                            text: "Сохранить",
+                            className: "popup-window-button-accept",
+                            events: {
+                                click: function () {
+                                    handleSavePromo()
+                                    this.popupWindow.close();
+                                }
+                            }
+                        }),
+                        new BX.PopupWindowButton({
+                            text: "Закрыть",
+                            className: "webform-button-link-cancel",
+                            events: {
+                                click: function () {
+                                    this.popupWindow.close();
+                                }
+                            }
+                        })
+                    ],
+                    events: {
+                        onPopupShow: function() {
+                            // Событие при показе окна
+                        },
+                        onPopupClose: function() {
+                            dropDown.classList.remove('--open')
+                        }
+                    }
+                });
+
+
+            dropDown.addEventListener('click', e => {
+                if(dropDown.classList.contains('--open')){
+                    promoPopup.close()
+                    dropDown.classList.remove('--open')
+                } else{
+                    promoPopup.show()
+                    dropDown.classList.add('--open')
+                }
+            })
+        })
+    </script>
